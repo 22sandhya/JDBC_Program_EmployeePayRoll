3b0ed45e506d92510e5414384c7679aef76b4ede
Admin <sandhyabagale10@gmail.com>
2021-11-11 16:59:27 +0530

    Added:welcome message

--- src/main/java/com/bridgelabz/JdbcProgram/EmployeePayroll.java
+++ src/main/java/com/bridgelabz/JdbcProgram/EmployeePayroll.java
@@ -1,23 +1,10 @@
 package com.bridgelabz.JdbcProgram;
 
+public class EmployeePayRoll {
 
-import java.sql.SQLException;
-import java.util.Scanner;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
 
-public class EmployeePayroll
-{
+	}
 
-    public static void main(String[] args) throws ClassNotFoundException, SQLException {
-        Scanner s = new Scanner(System.in);
-
-        System.out.println("Enter FirstName");
-        EmployeeDetails details = new EmployeeDetails();
-        details.setFirstName(s.next());
-
-        System.out.println("Enter LastName");
-        details.setLastName(s.next());
-
-        EmployeeRepo repo = new EmployeeRepo();
-        repo.insertRecord(details);
-    }
 }
